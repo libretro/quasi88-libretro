86a34be1843bf866bb4be3f7893f02baf8ee2910
celerizer <33245078+celerizer@users.noreply.github.com>
2019-03-18 19:01:09 -0500

    Android makefile

---
+++ src/LIBRETRO/jni/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+CORE_DIR   := $(LOCAL_PATH)/../../../src
+
+include $(CORE_DIR)/../Makefile.common
+
+COREFLAGS := -DHAVE_STDINT_H -DHAVE_INTTYPES_H -D__LIBRETRO__
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_CXX)
+LOCAL_CXXFLAGS  := $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
+include $(BUILD_SHARED_LIBRARY)
