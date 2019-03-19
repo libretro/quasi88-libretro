70d9c5c501454dbdb2effd94299f18e1a413659a
celerizer <33245078+celerizer@users.noreply.github.com>
2019-03-18 23:32:31 -0500

    android buildfixes

--- src/LIBRETRO/jni/Android.mk
+++ src/LIBRETRO/jni/Android.mk
@@ -9,5 +9,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := retro
 LOCAL_SRC_FILES := $(SOURCES_CXX)
 LOCAL_CXXFLAGS  := $(COREFLAGS)
-LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/../link.T
 include $(BUILD_SHARED_LIBRARY)
